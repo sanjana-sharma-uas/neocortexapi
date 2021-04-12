320f03cf185ba9fe5e532674854fa4e2706c0152
sanjana-sharma-uas <sanjana.sharma@stud.fra-uas.de>
2021-04-12 15:49:52 +0200

    Added a bit of bucketimplementation code

---
+++ NeoCortexApi/NeoCortexApi/ScalarEncoder2.cs
@@ -0,0 +1,27 @@
+﻿namespace NeoCortexApi
+{
+    public class ScalarEncoder2
+    {
+        public int[] getBucketIndices(String input)
+        {
+            TIntList l = new TIntArrayList();
+            Map<EncoderTuple, List<EncoderTuple>> encoders = getEncoders();
+            if (((encoders != null)
+                 && (encoders.size() > 0)))
+            {
+                foreach (EncoderTuple t in encoders.keySet())
+                {
+                    l.addAll(t.getEncoder().getBucketIndices(input));
+                }
+
+            }
+            else
+            {
+                throw new IllegalStateException(("Should be implemented in base classes that are not " +
+                                                 "containers for other encoders"));
+            }
+
+            return l.toArray();
+        }
+    }
+}
