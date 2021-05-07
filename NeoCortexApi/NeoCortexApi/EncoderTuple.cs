9c400a156060266fdab0048e0adf9bba2a1ab3ce
sanjana-sharma-uas <sanjana.sharma@stud.fra-uas.de>
2021-05-07 18:37:01 +0200

    Added and Implemented new class of Encoder Tuple

---
+++ NeoCortexApi/NeoCortexApi/EncoderTuple.cs
@@ -0,0 +1,46 @@
+﻿    using System;
+    using System.Text;
+    using NeoCortexApi.Entities;
+
+    namespace NeoCortexApi.Utility
+    {
+        public class EncoderTuple : Tuple
+        {
+
+            private static long serialVersionUID;
+        }
+
+        public class EncoderTuple
+        {
+            publicEncoderTuple(String, name, Encoder, <, Question, Greater, e, int, offset);
+            {
+                base.(name, e, offset);
+                if ((name == null))
+                {
+                    throw new ArgumentException(("Can\'t instantiate an EncoderTuple " + " with a null Name"));
+                }
+
+                if ((e == null))
+                {
+                    throw new ArgumentException(("Can\'t instantiate an EncoderTuple " + " with a null Encoder"));
+                }
+
+
+                public String getName()
+                {
+                    return ((String) (get(0)));
+                }
+
+                public Encoder getEncoder()
+                {
+                    return ((Encoder) (get(1)));
+                }
+
+                public int getOffset()
+                {
+                    return ((Integer) (get(2)));
+                }
+            }
+        }
+    }
+    
